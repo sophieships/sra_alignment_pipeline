b8c6971f6147c3477f039af8301775ca2478c69c
Sophie <81486685+sophieships@users.noreply.github.com>
2023-05-17 12:09:53 -0400

    update script to save run data in sep directories

--- scripts/archive_nf.sh
+++ scripts/archive_nf.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Define the directory where you want to move the work and log files
-archive_dir="nextflow_archive"
+# Define the base directory where you want to move the work and log files
+archive_base_dir="nextflow_archive"
 
-# Create the directory if it doesn't exist
-mkdir -p $archive_dir
+# Create the base directory if it doesn't exist
+mkdir -p $archive_base_dir
 
-# Move the work directory and the .nextflow.log file to the archive directory
-mv work $archive_dir/
-mv .nextflow.log* $archive_dir/
-mv .nextflow $archive_dir/
+# Create a unique subdirectory for this run using a timestamp
+run_dir="${archive_base_dir}/run_$(date +%Y%m%d_%H%M%S)"
 
+# Create the unique subdirectory
+mkdir -p $run_dir
+
+# Move the work directory, the .nextflow.log file, and other .nextflow* files to the run directory
+mv work $run_dir/
+mv .nextflow.log $run_dir/
+mv .nextflow* $run_dir/
